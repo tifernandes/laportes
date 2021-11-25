a6f4758999c4b7a4d858e2be1ee2ec9bc4cf7b2e
Tiago Fernandes <tiago@visumdigital.com>
2021-11-25 10:02:17 -0300

    test cors

--- pages/api/contatoAPI.js
+++ pages/api/contatoAPI.js
@@ -5,16 +5,6 @@ const sendEmail = require('../../components/SendEmail');
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req, res) {
 
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-
     if(req.method === 'POST'){
 
       const data = req.body;
--- pages/api/pedidosAPI.js
+++ pages/api/pedidosAPI.js
@@ -4,16 +4,6 @@ import { MongoClient, ObjectId } from 'mongodb';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req, res) {
 
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-
     if(req.method === 'POST'){
 
       const data = req.body;
